d93e05cb74d1f33e856dfda7de2369a93969c839
PavelSolovev <pashius2010@gmail.com>
2018-02-13 22:34:46 +0300

    add map.js

---
+++ js/map.js
@@ -0,0 +1,12 @@
+let ads = [
+  {
+    "author": {
+      "avatar": "img/avatars/user01.png"
+    },
+    "offer": {
+      "title": "Большая уютная квартира",
+      "address": "{{location.x}}, {{location.y}}"
+    }
+  }
+];
+
