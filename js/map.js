84969799a33663de1e1f33c3b721ce1759c62454
SPavelV <pashius2010@gmail.com>
2018-02-14 12:05:02 +0400

    Исходные данные для отрисовки объектов на карте

--- js/map.js
+++ js/map.js
@@ -1,12 +1,26 @@
-let ads = [
-  {
-    "author": {
-      "avatar": "img/avatars/user01.png"
-    },
-    "offer": {
-      "title": "Большая уютная квартира",
-      "address": "{{location.x}}, {{location.y}}"
-    }
-  }
-];
+"use strict";
 
+class Ads {
+
+  constructor() {
+    const MAX_OBJECT = 8;
+    const MIN_PRICE = 1000;
+    const MAX_PRICE = 1000000;
+    const MAX_ROOMS = 5;
+    const MAX_GUESTS = 10;
+    const getRandomNum = num => {
+      return Math.ceil((Math.random() * num) );
+    };
+    const TITLE_OBJECT = ["Большая уютная квартира", "Маленькая неуютная квартира",
+                   "Огромный прекрасный дворец", "Маленький ужасный дворец",
+                   "Красивый гостевой домик", "Некрасивый негостеприимный домик",
+                   "Уютное бунгало далеко от моря", "Неуютное бунгало " +
+                   "по колено в воде"];
+    const TYPE_OBJECT = ['flat', 'house', 'bungalo'];
+    let ROOMS = getRandomNum(MAX_ROOMS);
+    let guests = getRandomNum(MAX_GUESTS);
+    let address =  "{{location.x}}, {{location.y}}";
+    let avatarSrc = getRandomNum(MAX_OBJECT);
+    let adsObjectArray = [];
+  }
+}
