50a663b5a4ef61a160ac37ceb649511879ea1fac
SPavelV <pashius2010@gmail.com>
2018-04-06 11:09:25 +0400

    Добавил перетаскивание пина на карте

--- js/map.js
+++ js/map.js
@@ -228,6 +228,42 @@ class Map {
               activePin(pinAll, pin, evt, i);
             }
           });
+
+          item.addEventListener('mousedown', (evt) => {
+            evt.preventDefault();
+
+            let startCoords = {
+              x: evt.clientX,
+              y: evt.clientY
+            };
+
+                const onMouseMove = (moveEvt) => {
+              moveEvt.preventDefault();
+
+              let shift = {
+                x: startCoords.x - moveEvt.clientX,
+                y: startCoords.y - moveEvt.clientY
+              };
+
+              startCoords = {
+                x: moveEvt.clientX,
+                y: moveEvt.clientY
+              };
+
+              moveEvt.target.style.top = (moveEvt.target.offsetTop - shift.y) + 'px';
+              moveEvt.target.style.left = (moveEvt.target.offsetLeft - shift.x) + 'px';
+            };
+
+            const onMouseUp = (upEvt) => {
+              upEvt.preventDefault();
+
+              document.removeEventListener('mousemove', onMouseMove);
+              document.removeEventListener('mouseup', onMouseUp);
+            };
+
+            document.addEventListener('mousemove', onMouseMove);
+            document.addEventListener('mouseup', onMouseUp);
+          })
         });
 
         openDialogPanel();
